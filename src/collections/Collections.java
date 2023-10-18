dd5152321e6b7264b430e5355692b8ce62cb596c
2022345 <2022345@student.cct.ie>
2023-10-18 14:10:55 +0100

    Created methods and subclass rectangle

--- src/collections/Collections.java
+++ src/collections/Collections.java
@@ -4,6 +4,8 @@
  */
 package collections;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author suele
@@ -15,6 +17,47 @@ public class Collections {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Rectangle[] rectangles = {new Rectangle (2, 3), new Rectangle (4, 1)};
+        for (int rectangle = 0; rectangle < rectangles.length; rectangle ++){
+            System.out.println(rectangles[rectangle].length);
+            
+        }
+        
+        for (Rectangle rectangle : rectangles){
+            System.out.println(rectangles.width);
+        }
+        
+        ArrayList<Integer> numList = new ArrayList <>();
+        
+        numList.add(5);
+        numList.add(10);
+        
+        numList.add(0,3);
+        numList.clear();
+        numList.add(5);
+        numList.contains(4);
+        numList.set(0,10);
+        numList.add(5);
+        numList.set(0,7);
+        numList.set(1,2);
+        
+        
+        ArrayList<String> stringList = new ArrayList<>();
+        
+        stringList.add("Hello!");
+        stringList.add("Hello!");
+        stringList.add("World");
+        
+          stringList.set(0, "World");
+          
+          stringList.remove("World");
+          
+        //  while (stringList.contains("World")){
+         //     stringList.remove("World");
+        //  }
+        
+        stringList.remove(1);
     }
     
 }
